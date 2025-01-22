e4c53849bef77037f4db77ea19c92269d8a36b1b
mbg206 <66435380+mbg206@users.noreply.github.com>
2025-01-22 16:58:10 -0600

    fix

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,9 @@
+(async()=>{
+if(typeof students=="object")return;
 const r=await fetch("https://mbg206.github.io/cn-kiosk-better/");
 const t=await r.text();
 const d=document;
 d.open();
 d.write(t);
 d.close();
+})();
